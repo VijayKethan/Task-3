c8de7e9eb19a0702ea440818046b2c97814b6298
VijayKethan <neonlights.dream@gmail.com>
2017-10-10 16:44:32 +0530

    Initial commit

---
+++ app/src/main/java/com/example/vcarve_dt_14/task_3/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.vcarve_dt_14.task_3;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bottom_navigation);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()) {
+
+                    case R.id.action_left:
+
+                        case R.id.action_center:
+
+                            case R.id.action_right:
+
+                }
+
+                return true;
+            }
+        });
+    }
+}
